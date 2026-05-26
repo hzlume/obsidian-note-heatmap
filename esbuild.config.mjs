477a2e855dfbac3e6517e101c38b85691090b4e2
lu <lu@ludeMacBook-Pro.local>
2026-05-26 21:46:31 +0800

    fix: resolve community plugin review warnings
    - Remove unused eslint-plugin-react dependency
    - Replace builtin-modules with static Node.js builtins list
    - Remove !important from CSS, raise selector specificity
    - Replace 'Coming Soon' placeholder in README
    
    Closes HZL-10

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,13 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from "builtin-modules";
+const builtins = [
+	"assert", "async_hooks", "buffer", "child_process", "cluster", "console",
+	"constants", "crypto", "dgram", "dns", "domain", "events", "fs", "http",
+	"http2", "https", "inspector", "module", "net", "os", "path", "perf_hooks",
+	"process", "punycode", "querystring", "readline", "repl", "stream",
+	"string_decoder", "sys", "timers", "tls", "trace_events", "tty", "url",
+	"util", "v8", "vm", "wasi", "worker_threads", "zlib"
+];
 import fs from "fs";
 import path from "path";
 
